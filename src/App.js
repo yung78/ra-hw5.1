bd1fd4cb16745f9d9dbbedae31f288f1ad64cb4e
yung78 <alexdrum@mail.ru>
2023-10-30 14:09:52 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Card from './components/Card';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card 
+        title={'Card title'}
+        text={"Some quick example text to build on the card title and make up the bulk of the card's content."}
+      >
+        <img src="./img.jpg" className="card-img-top" alt="card_img"/>
+      </Card>
+      <Card 
+        title={'Special title treatment'}
+        text={'With supporting text below as a natural lead-in to additional content.'}
+        />
     </div>
   );
 }
